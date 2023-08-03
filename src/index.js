e2ed5eb832cf9c60999878faf8fd1378eeb1c89e
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-03 06:20:08 +0200

    added_category_select_function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,42 @@ import { getCategories } from './js/books_api.js';
 import { getBookByCategory } from './js/books_api.js';
 
 const categorySection = document.querySelector('.book-kategories-js');
+const categorySectionList = document.querySelector('.book-kategories-list-js');
+const sectionSelectedBooksByCategory =
+  document.querySelector('.books-by-category');
+categorySection.addEventListener('click', pushBooksByCategory);
 
-const bookCategoryClass = categorySection.children;
-console.log(bookCategoryClass);
+function pushBooksByCategory(e) {
+  const selectedCategory = e.target.outerText;
+  let booksArr = [];
+  getBookByCategory()
+    .then(resp => {
+      const filteredBooks = resp
+        .filter(book => {
+          return book.list_name === selectedCategory;
+        })
+        .map(book => book.books);
+      booksArr.push(...filteredBooks);
+      const filteredByCategoryBooks = booksArr[0];
+      filteredByCategoryBooks.map(book => {
+        console.log(book);
+        sectionSelectedBooksByCategory.insertAdjacentHTML('beforeend',`<div>
+        <img src="${book.book_image}" alt="">
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>
+    </div>` );
+      });
+    })
+    .catch(err => console.log(err));
+}
+
 getCategories()
   .then(resp => {
     if (!resp) {
       throw new Error('error');
     }
-    console.log(resp);
     return resp.map(category =>
-      categorySection.insertAdjacentHTML(
+      categorySectionList.insertAdjacentHTML(
         'beforeend',
         `<li class ="book-category">${category.list_name}</li>`
       )
@@ -25,6 +50,5 @@ getBookByCategory()
     if (!resp) {
       throw new Error('error');
     }
-    console.log(resp);
   })
   .catch(err => console.log(err));
