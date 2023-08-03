61518d643d6b924f67dcfb36abcc0d116fad3eef
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-03 04:02:15 +0200

    added_2nd_function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { getCategories } from './js/books_api.js';
+import { getBookByCategory } from './js/books_api.js';
 
 const categorySection = document.querySelector('.book-kategories-js');
 
+const bookCategoryClass = categorySection.children;
+console.log(bookCategoryClass);
 getCategories()
   .then(resp => {
     if (!resp) {
@@ -11,8 +14,17 @@ getCategories()
     return resp.map(category =>
       categorySection.insertAdjacentHTML(
         'beforeend',
-        `<h3>${category.list_name}</h3>`
+        `<li class ="book-category">${category.list_name}</li>`
       )
     );
+  })
+  .catch(err => console.log(err));
+
+getBookByCategory()
+  .then(resp => {
+    if (!resp) {
+      throw new Error('error');
+    }
+    console.log(resp);
   })
   .catch(err => console.log(err));
--- src/js/books_api.js
+++ src/js/books_api.js
@@ -1,7 +1,13 @@
+const API = 'https://books-backend.p.goit.global';
+
 export async function getCategories() {
-  const response = await fetch(
-    'https://books-backend.p.goit.global/books/category-list'
-  );
+  const response = await fetch(`${API}/books/category-list`);
   const category = await response.json();
   return category;
+}
+
+export async function getBookByCategory() {
+  const response = await fetch(`${API}/books/top-books`);
+  const bookByCategory = await response.json()
+  return bookByCategory
 }
