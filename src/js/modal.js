9a1d3d1e96fd0be81353b39cde3ae89b4c86aab0
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-06 18:54:45 +0200

    disabled_scroll

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,9 @@
 export function openModal() {
     document.getElementById("myModal").style.display = "block";
+    document.body.classList.add('body-scroll-lock')
   }
   export function closeModal() {
     document.getElementById("myModal").style.display = "none";
+    document.body.classList.remove('body-scroll-lock')
+
   }  
