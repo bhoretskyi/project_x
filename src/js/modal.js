836ee0be67087313d8903fb4c77cde9295fada0b
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-06 19:10:32 +0200

    change_class

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,11 +3,11 @@ const backdrop = document.querySelector('.backdrop');
 export function openModal() {
   document.getElementById('myModal').style.display = 'block';
   document.body.classList.add('body-scroll-lock');
-  backdrop.classList.add('active');
+  backdrop.classList.add('modal-active');
 }
 export function closeModal() {
   document.getElementById('myModal').style.display = 'none';
   document.body.classList.remove('body-scroll-lock');
-  backdrop.classList.remove('active')
+  backdrop.classList.remove('modal-active')
 }
  
