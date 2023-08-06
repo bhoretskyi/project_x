7a0af24b3944480834a503cfa134c41999be8358
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-06 17:30:08 +0200

    added_trash_buttons

--- src/js/shop.js
+++ src/js/shop.js
@@ -3,6 +3,7 @@ const shopListBookSection = document.querySelector('.shop-list-books-section');
 const amazon = document.querySelector('.amazon');
 const ios = document.querySelector('.book-ios');
 const shop = document.querySelector('.book-shop');
+const trashSvg = document.querySelector('.svg')
 
 const savedBooks = JSON.parse(localStorage.getItem('books'));
 function paintBooksFromLocalstorage() {
@@ -10,18 +11,21 @@ function paintBooksFromLocalstorage() {
     getBookById(book)
       .then(resp => {
         if (!resp) {
-          throw new Error('error');
+          throw new Error('error'); 
         }
 
-        shopListBookSection.insertAdjacentHTML(
+        shopListBookSection.insertAdjacentHTML( 
           'beforeend',
           `<div><p hidden>${resp._id}</p>
-        <img  src="${resp.book_image}" alt="">
-        
+    <button type="button" class="trash-btn"><img  src="${trashSvg.src}" alt=""></button>
+
+        <img  src="${resp.book_image}" alt=""> 
+         
       <h4>${resp.title}</h4>
       <p>${resp.author}</p>
-      <p>${resp.description}</p> 
-      <a href="${resp.buy_links[0].url}" target="_blank"><img src="${amazon.src}" alt=""></a>
+
+      <p>${resp.description}</p>  
+      <a href="${resp.buy_links[0].url}" target="_blank"><img src="${amazon.src}" alt=""></a> 
       <a href="${resp.buy_links[1].url}" target="_blank"><img src="${ios.src}" alt=""></a>
       <a href="${resp.buy_links[4].url}" target="_blank"><img src="${shop.src}" alt=""></a>
     
