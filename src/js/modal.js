a900c20076361268c90ae955431763fcac568580
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-06 19:09:26 +0200

    add_backdrop

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,13 @@
-export function openModal() {
-    document.getElementById("myModal").style.display = "block";
-    document.body.classList.add('body-scroll-lock')
-  }
-  export function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-    document.body.classList.remove('body-scroll-lock')
+const backdrop = document.querySelector('.backdrop');
 
-  }  
+export function openModal() {
+  document.getElementById('myModal').style.display = 'block';
+  document.body.classList.add('body-scroll-lock');
+  backdrop.classList.add('active');
+}
+export function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+  document.body.classList.remove('body-scroll-lock');
+  backdrop.classList.remove('active')
+}
+ 
