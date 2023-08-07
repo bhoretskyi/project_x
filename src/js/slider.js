747c49dbcb3c5267afb26295e5d8b686e8ae5ce1
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-08-07 16:44:32 +0300

    Сделал слайдер телефонную версию

---
+++ src/js/slider.js
@@ -0,0 +1,8 @@
+$(function(){
+            $('.slider').slick({
+                vertical: true,
+                verticalSwiping: true,
+                slidesToShow: 4,
+                autoplay: false,
+            });
+        });
