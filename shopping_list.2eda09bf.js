f3b55e20e531c634b8ca38dfbd592b290a2d944f
bhoretskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-08 21:02:36 +0000

    Deploying to gh-pages from @ bhoretskyi/project_x@defd1927078c932ec6555193f31fb9f823ee7d14 🚀

--- shopping_list.2eda09bf.js
+++ shopping_list.2eda09bf.js
@@ -1,2 +1,2 @@
 $((function(){$(".slider").slick({vertical:!0,verticalSwiping:!0,slidesToShow:6,autoplay:!1})}));
-//# sourceMappingURL=index.cc7159e0.js.map
+//# sourceMappingURL=shopping_list.2eda09bf.js.map
--- shopping_list.f9efa157.js
+++ shopping_list.f9efa157.js
@@ -1,2 +1,2 @@
 $((function(){$(".slider").slick({vertical:!0,verticalSwiping:!0,slidesToShow:6,autoplay:!1})}));
-//# sourceMappingURL=index.6e042768.js.map
+//# sourceMappingURL=shopping_list.f9efa157.js.map
