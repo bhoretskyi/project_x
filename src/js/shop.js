c500099787cf22ebe3ebb92943cf5c67d18208cc
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-09 19:23:52 +0200

    fixed_menu

--- src/js/shop.js
+++ src/js/shop.js
@@ -6,24 +6,24 @@ const amazon = document.querySelector('.amazon');
 const ios = document.querySelector('.book-ios');
 const shop = document.querySelector('.book-shop');
 const trashSvg = document.querySelector('.svg');
-const homeBtn = document.querySelector('.home-btn')
-homeBtn.classList.remove('current')
-const shoplistBtn = document.querySelector('.list-btn')
-shoplistBtn.classList.add('current')
+const homeBtn = document.querySelector('.home-btn');
+homeBtn.classList.remove('current');
+const shoplistBtn = document.querySelector('.list-btn');
+shoplistBtn.classList.add('current');
 
 const savedBooks = JSON.parse(localStorage.getItem('books'));
 function paintBooksFromLocalstorage() {
   savedBooks.map(book => {
     getBookById(book)
       .then(resp => {
-        Loading.remove()
+        Loading.remove();
         if (!resp) {
           throw new Error('error');
         }
-        let shortdescription = ''
-        const words = resp.description.split(' ')
+        let shortdescription = '';
+        const words = resp.description.split(' ');
         if (words.length > 10) {
-          shortdescription = words.slice(0, 10).join(' ') + '...';  
+          shortdescription = words.slice(0, 10).join(' ') + '...';
         }
 
         shopListBookSection.insertAdjacentHTML(
@@ -63,26 +63,24 @@ function paintBooksFromLocalstorage() {
       
 `
         );
-        
-        
       })
       .catch(err => {
-        Notiflix.Notify.failure('Oops... something went wrong. Please reload the page')
-        console.log(err)});
+        Notiflix.Notify.failure(
+          'Oops... something went wrong. Please reload the page'
+        );
+        console.log(err);
+      });
   });
 }
 
 paintBooksFromLocalstorage();
 
-shopListBookSection.addEventListener('click', e => {  
+shopListBookSection.addEventListener('click', e => {
   if (e.target.className === 'trash-js') {
-
-
-  
     const bookToRemoveId =
-    e.target.parentElement.parentElement.lastElementChild.textContent
-    const newSavedBooks = savedBooks.filter(item => item !== bookToRemoveId);    
-    localStorage.setItem('books', JSON.stringify(newSavedBooks)); 
+      e.target.parentElement.parentElement.lastElementChild.textContent;
+    const newSavedBooks = savedBooks.filter(item => item !== bookToRemoveId);
+    localStorage.setItem('books', JSON.stringify(newSavedBooks));
     location.reload();
   }
 });
@@ -94,22 +92,20 @@ shopListBookSection.addEventListener('click', e => {
 //   console.log(e.target);
 // });
 
-
-
-const checkBox = document.querySelector('.checkbox')
- //console.log(loginForm);
+const checkBox = document.querySelector('.checkbox');
+//console.log(loginForm);
 const svgIconHeader = document.querySelector('.icon-bookshelf');
 const listItemQ = document.querySelector('.book-categories-list');
 const svgIconShop = document.querySelector('.list-btn-svg');
 const headerFone = document.querySelector('.header-container');
-const section = document.querySelector('.shop-list-books-section')
-const shopTitleGen = document.querySelector('.shop-list-section-title')
+const section = document.querySelector('.shop-list-books-section');
+const shopTitleGen = document.querySelector('.shop-list-section-title');
 //console.log(shoppingListtitle.textContent);
 
 checkBox.addEventListener('change', chengeThemeShopp);
 
 function chengeThemeShopp() {
-  console.log('Клик работает')
+  console.log('Клик работает');
   document.body.classList.toggle('dark-thema');
   svgIconHeader.classList.toggle('svg-icon-header');
   svgIconShop.classList.toggle('svg-icon-header');
@@ -119,17 +115,15 @@ function chengeThemeShopp() {
   const cardFone = section.getElementsByClassName('shopping-list-card');
   console.log(cardFone);
   for (const card of cardFone) {
-    card.classList.toggle('shopping-card'); 
-    const textTitle = card.getElementsByClassName('shopping-list-title')
+    card.classList.toggle('shopping-card');
+    const textTitle = card.getElementsByClassName('shopping-list-title');
     console.log(textTitle);
     for (const title of textTitle) {
-      title.classList.toggle('title-color')
+      title.classList.toggle('title-color');
     }
   }
-  console.log(cardFone)
-  
-
-} 
+  console.log(cardFone);
+}
 
 const burgerBtn = document.querySelector('.js-burger');
 const burgerCloseBtn = document.querySelector('.js-close-menu');
@@ -145,23 +139,20 @@ burgerCloseBtn.addEventListener('click', () => {
   burgerBtn.hidden = false;
 });
 
-function chekWindowSize () {
+function chekWindowSize() {
   if (window.innerWidth >= 768) {
-    closeBurgerModal()
+    closeBurgerModal();
     burgerCloseBtn.classList.add('is-hidden-btn');
-  burgerBtn.hidden = false;
-    
+    burgerBtn.hidden = false;
   }
 }
-window.addEventListener('resize', chekWindowSize)
+window.addEventListener('resize', chekWindowSize);
 
-
-
-$(function(){
-            $('.slider').slick({
-                vertical: true,
-                verticalSwiping: true,
-                slidesToShow: 6,
-                autoplay: false,
-            });
-        });
+$(function () {
+  $('.slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 6,
+    autoplay: false,
+  });
+});
