ec63db4bf1f817941e6a47473c888ef4e7383f37
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-08-09 14:26:45 +0300

    Получилось!!!!!!!!! Количество слайдов меняется

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,48 @@
-$(function(){
+$(function () {
             $('.slider').slick({
                 vertical: true,
                 verticalSwiping: true,
                 slidesToShow: 6,
                 autoplay: false,
+                responsive: [
+     {
+       breakpoint: 768,
+       settings: {
+         slidesToShow: 4,
+       }
+     },
+     {
+       breakpoint: 375,
+       settings: {
+         slidesToShow: 1,
+       }
+     }
+    ]
             });
         });
+
+
+
+// window.addEventListener('resize', checkWidth);
+
+// function checkWidth() {
+    
+
+//     if (window.innerWidth < 376) {
+    
+
+// } else {
+//     $(function () {
+//             $('.slider').slick({
+//                 vertical: true,
+//                 verticalSwiping: true,
+//                 slidesToShow: 6,
+//                 autoplay: false,
+//             });
+//         });
+//     }
+    
+// }
+
+
+
---
+++ src/js/slider2.js
@@ -0,0 +1,8 @@
+// $(function () {
+//             $('.slider').slick({
+//                 vertical: true,
+//                 verticalSwiping: true,
+//                 slidesToShow: 6,
+//                 autoplay: false,
+//             });
+//         });
